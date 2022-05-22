ee2b456ac111fc1f011eec43298d70c414050421
Robert Bilyk <bilykr@mail.gvsu.edu>
2022-05-22 23:28:36 +0000

    finished final graph start of concealing

--- Plot Work/First Graph.R
+++ Plot Work/First Graph.R
@@ -19,14 +19,17 @@ data <- read_xlsx(here::here("Plot Work", "Henry Gannett Total State Population 
 data$State <- toupper(data$State)
 data$pop <- data$Population / 100000
 
-x <- ggplot(data, aes(x = pop, y = fct_rev(fct_inorder(State))), fill = NA) +
+
+#Full Graph
+
+ggplot(data, aes(x = pop, y = fct_rev(fct_inorder(State))), fill = NA) +
   geom_col(color = "black", fill = "black", width = 0.45, ) +
-  labs(title = "POPULATION OF EACH STATE AND TERRITORY: 1890",
+  labs(title = "6. POPULATION OF EACH STATE AND TERRITORY: 1890",
        subtitle = element_blank(),
        tag = "(Hundreds of thousands.)") +
   scale_fill_manual(values = c("#f1d9b5", "black")) +
   theme_bw() +
-  theme(axis.text.x = element_text(family = "Main", size = 8),
+  theme(axis.text.x = element_text(family = "Main", size = 8, color= "black"),
         axis.title.x = element_blank(), 
         axis.ticks.x = element_line()) + 
   theme(axis.ticks.y = element_blank(), 
@@ -39,23 +42,94 @@ x <- ggplot(data, aes(x = pop, y = fct_rev(fct_inorder(State))), fill = NA) +
         panel.grid.major.y = element_blank(),
         panel.background = element_blank(),
         plot.background = element_rect(linetype = 1, size = 1, fill = "#f1d9b5"), 
-        plot.title = element_text(family = "Main", color = "black", hjust = .25, vjust = 4, size = 8),
+        plot.title = element_text(family = "Main", color = "black", hjust = .23, vjust = 5, size = 7),
         plot.subtitle = element_blank(),
         plot.margin = margin(10,10,10,10),
-        plot.tag = element_text(family = "Main", color = "black", hjust = .5, vjust = -6, size = 6),
+        plot.tag = element_text(family = "Main", color = "black", hjust = .5, vjust = -7, size = 5),
         plot.tag.position = "top",
         legend.position = "none") + 
-        scale_x_continuous(position = "top", limits = c(0,62), breaks=seq(0,60,6)) +
+        scale_x_continuous(position = "top", limits = c(0,62), breaks=seq(6,60,6)) +
         coord_cartesian(clip = "off") 
-print(x)
 
+grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.01, y0 = 0.025, y1 = 0.94))
+grid.draw(segmentsGrob(x0 = 0.935, x1 = 0.935, y0 = 0.025, y1 = 0.94))
+grid.draw(segmentsGrob(x0 = 0.245, x1 = 0.245, y0 = 0.025, y1 = 0.94))
+grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.935, y0 = 0.915, y1 = 0.915))
+grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.935, y0 = 0.94, y1 = 0.94))
+grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.935, y0 = 0.025, y1 = 0.025))
+grid.draw(textGrob("STATES.", x=.13,y=.926, gp=gpar(fontsize=8, fontfamily="Main")))
 
-grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.01, y0 = 0.02, y1 = 0.91))
-grid.draw(segmentsGrob(x0 = 0.935, x1 = 0.935, y0 = 0.02, y1 = 0.91))
-grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.935, y0 = 0.91, y1 = 0.91))
-grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.935, y0 = 0.02, y1 = 0.02))
-grid.draw(textGrob("STATES.", x=.1,y=.923, gp=gpar(fontsize=8, fontfamily="Main")))
+#Fully Concealed
 
+ggplot(data, aes(x = pop, y = fct_rev(fct_inorder(State))), fill = NA) +
+  geom_col(color = "black", fill = "black", width = 0.45, ) +
+  labs(title = "6. POPULATION OF EACH STATE AND TERRITORY: 1890",
+       subtitle = element_blank(),
+       tag = "(Hundreds of thousands.)") +
+  scale_fill_manual(values = c("#f1d9b5", "black")) +
+  theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(), 
+        axis.ticks.x = element_blank()) + 
+  theme(axis.ticks.y = element_blank(), 
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank()) + 
+  theme(panel.border = element_blank(),
+        axis.ticks = element_line(size = .25),
+        panel.grid.major = element_line(color="black", size=.25),
+        panel.grid.minor = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.background = element_blank(),
+        plot.background = element_rect(linetype = 1, size = 1, fill = "#f1d9b5"), 
+        plot.title = element_blank(),
+        plot.subtitle = element_blank(),
+        plot.margin = margin(10,10,10,10),
+        plot.tag = element_blank(),
+        plot.tag.position = "top",
+        legend.position = "none") + 
+  scale_x_continuous(position = "top", limits = c(0,62), breaks=seq(6,60,6)) +
+  coord_cartesian(clip = "off") 
 
-rect
+grid.draw(segmentsGrob(x0 = 0.075, x1 = 0.075, y0 = 0.025, y1 = 0.97))
+grid.draw(segmentsGrob(x0 = 0.935, x1 = 0.935, y0 = 0.025, y1 = 0.97))
+grid.draw(segmentsGrob(x0 = 0.075, x1 = 0.935, y0 = 0.025, y1 = 0.025))
+grid.draw(segmentsGrob(x0 = 0.075, x1 = 0.935, y0 = 0.97, y1 = 0.97))
 
+#Reveal Population Numbers
+
+ggplot(data, aes(x = pop, y = fct_rev(fct_inorder(State))), fill = NA) +
+  geom_col(color = "black", fill = "black", width = 0.45, ) +
+  labs(title = "6. POPULATION OF EACH STATE AND TERRITORY: 1890",
+       subtitle = element_blank(),
+       tag = "(Hundreds of thousands.)") +
+  scale_fill_manual(values = c("#f1d9b5", "black")) +
+  theme_bw() +
+  theme(axis.text.x = element_text(family = "Main", size = 8, color= "black"),
+        axis.title.x = element_blank(), 
+        axis.ticks.x = element_line()) + 
+  theme(axis.ticks.y = element_blank(), 
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank()) + 
+  theme(panel.border = element_blank(),
+        axis.ticks = element_line(size = .25),
+        panel.grid.major = element_line(color="black", size=.25),
+        panel.grid.minor = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.background = element_blank(),
+        plot.background = element_rect(linetype = 1, size = 1, fill = "#f1d9b5"), 
+        plot.title = element_text(family = "Main", color = "black", hjust = .23, vjust = 5, size = 7),
+        plot.subtitle = element_blank(),
+        plot.margin = margin(10,10,10,10),
+        plot.tag = element_text(family = "Main", color = "black", hjust = .5, vjust = -7, size = 5),
+        plot.tag.position = "top",
+        legend.position = "none") + 
+  scale_x_continuous(position = "top", limits = c(0,62), breaks=seq(6,60,6)) +
+  coord_cartesian(clip = "off") 
+
+grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.01, y0 = 0.025, y1 = 0.94))
+grid.draw(segmentsGrob(x0 = 0.935, x1 = 0.935, y0 = 0.025, y1 = 0.94))
+grid.draw(segmentsGrob(x0 = 0.245, x1 = 0.245, y0 = 0.025, y1 = 0.94))
+grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.935, y0 = 0.915, y1 = 0.915))
+grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.935, y0 = 0.94, y1 = 0.94))
+grid.draw(segmentsGrob(x0 = 0.01, x1 = 0.935, y0 = 0.025, y1 = 0.025))
+grid.draw(textGrob("STATES.", x=.13,y=.926, gp=gpar(fontsize=8, fontfamily="Main")))
