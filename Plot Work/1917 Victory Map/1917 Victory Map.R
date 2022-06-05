b691f829034b04401d8f37da5328f1a13bbf7d2d
Robert Bilyk <bilykr@mail.gvsu.edu>
2022-06-05 05:29:50 +0000

    further work

--- Plot Work/1917 Victory Map/1917 Victory Map.R
+++ Plot Work/1917 Victory Map/1917 Victory Map.R
@@ -30,14 +30,17 @@ ggplot(data = newmap) +
                    pattern_spacing = Type,
                    pattern_type = Type,
                    pattern_filename = Type,
-                   pattern_scale = Type),
+                   pattern_scale = Type,
+                   pattern_density = Type),
                color="grey") +
   scale_fill_manual(values = c("white","black","white","white","white","black","white")) +
   scale_pattern_manual(values = c("none", "circle", "image", "circle", "stripe", "none", "image")) +
   scale_pattern_fill_manual(values = c("white", "white", "black", "black", "white", "white", "white")) +
-  scale_pattern_spacing_manual(values = c(.05, .025, .05, .025, .05, .05, .05)) +
-  scale_pattern_type_manual(values = c(NA, NA, "tile", NA, NA, NA, "squish")) +
+  scale_pattern_spacing_manual(values = c(.05, .02, .05, .02, .0, .05, .05)) +
+  scale_pattern_type_manual(values = c(NA, NA, "tile", NA, NA, NA, "tile")) +
   scale_pattern_filename_manual(values = c(NA, NA, x_dot, NA, NA, NA, x_cross)) +
-  scale_pattern_scale_manual(values = c(1, 1, .09, 1, 1, 1, 1))
+  scale_pattern_scale_manual(values = c(1, 1, .06, 1, 1, 1, .06)) +
+  scale_pattern_density_manual(values = c(.2, .3, .2, .2, .2, .2,.2))
+
 
 dev.off()
