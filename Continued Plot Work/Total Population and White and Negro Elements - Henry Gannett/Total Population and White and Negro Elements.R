fc804a085fc11b7da1f44952f1a71633bc195e59
Robert Bilyk <bilykr@mail.gvsu.edu>
2022-06-07 04:49:59 +0000

    finished Total/White/Negro Population

--- Continued Plot Work/Total Population and White and Negro Elements - Henry Gannett/Total Population and White and Negro Elements.R
+++ Continued Plot Work/Total Population and White and Negro Elements - Henry Gannett/Total Population and White and Negro Elements.R
@@ -24,9 +24,22 @@ value <- c(1, 3, 1.4, 3.8, 1.5, 5.7, 1.9, 8, 2.5, 10.3, 3.0, 14, 3.5, 19.6,
 
 df <- data.frame(year, condition, value)
 
+year2 <- c("1790", "1800", "1810", "1820","1830",
+           "1840", "1850","1860","1870",
+           "1880", "1890")
+
+value2 <- c(4, 5.2, 7.2, 9.9, 12.8, 17, 23.1, 31.5, 38.5, 50.1, 62.5)
+
+df2 <- data.frame(year2, value2)
+
 x_axis_text <- c("", 0, 10, 20, 30, 40, 50, 60, "")
 
-# stacked bar graph
+
+
+# full graph
+
+png(here::here("Continued Plot Work", "Total Population and White and Negro Elements - Henry Gannett", "5-Final-TotalPWN-Pop.png"),width=900,height=700)
+
 ggplot(df, aes(fill = condition, x = value, y = reorder(year, -value)))  +
   geom_vline(xintercept = 0:70 , linetype="solid", 
              color = "gray21", size = 0.6) +
@@ -56,3 +69,157 @@ ggplot(df, aes(fill = condition, x = value, y = reorder(year, -value)))  +
         panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "white", colour = "white"),
         plot.background = element_rect(fill = "white", colour = "white"))
+
+dev.off()
+
+
+# full concealed
+
+png(here::here("Continued Plot Work", "Total Population and White and Negro Elements - Henry Gannett", "1-Concealed-TotalPWN-Pop.png"),width=900,height=700)
+
+ggplot(df2, aes(x = value2, y = reorder(year2, -value2), fill="white"))  +
+  geom_vline(xintercept = 0:70 , linetype="solid", 
+             color = "gray21", size = 0.6) +
+  geom_vline(xintercept = c(10,20,30,40,50,60,70), linetype="solid", 
+             color = "gray21", size = 1.2) +
+  geom_col(position = position_stack(reverse = TRUE), colour = "black", width = 0.5) +
+  scale_fill_manual(values = c("white", "white")) +
+  scale_y_discrete() +
+  scale_x_continuous(labels = x_axis_text, expand = c(0,0), position = "top", limits = c(-8,70), 
+                     breaks = seq(-10, 70, by = 10)) +
+  labs(title = "TOTAL POPULATION AND WHITE AND NEGRO ELEMENTS.",
+       subtitle = "PLATE I.") +
+  labs(y = NULL, x = "Millions.") +
+  theme(plot.title = element_text(hjust = 0.5, family = "Puritan", size = 15, color="white"),
+        plot.subtitle = element_text(hjust = 1, family = "Puritan", color="white")) +
+  theme(axis.text.x = element_text(colour = "white", 
+                                   face = "bold", family = "Dynalight", size = 15),
+        axis.text.y = element_text(vjust = -0.01,
+                                   hjust = 0.05,
+                                   margin = margin(l = 40, r = -60), colour = "white", 
+                                   face = "bold", family = "Puritan", size = 12)) +
+  theme(axis.title.x = element_text(color="white")) +
+  theme(axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank()) +
+  theme(legend.position = "none") +
+  theme(panel.border = element_rect(fill = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white", colour = "white"),
+        plot.background = element_rect(fill = "white", colour = "white"))
+
+dev.off()
+
+
+# reveal population
+
+png(here::here("Continued Plot Work", "Total Population and White and Negro Elements - Henry Gannett", "2-RevealPop-TotalPWN-Pop.png"),width=900,height=700)
+
+ggplot(df2, aes(x = value2, y = reorder(year2, -value2), fill="white"))  +
+  geom_vline(xintercept = 0:70 , linetype="solid", 
+             color = "gray21", size = 0.6) +
+  geom_vline(xintercept = c(10,20,30,40,50,60,70), linetype="solid", 
+             color = "gray21", size = 1.2) +
+  geom_col(position = position_stack(reverse = TRUE), colour = "black", width = 0.5) +
+  scale_fill_manual(values = c("white", "white")) +
+  scale_y_discrete() +
+  scale_x_continuous(labels = x_axis_text, expand = c(0,0), position = "top", limits = c(-8,70), 
+                     breaks = seq(-10, 70, by = 10)) +
+  labs(title = "TOTAL POPULATION",
+       subtitle = "PLATE I.") +
+  labs(y = NULL, x = "Millions.") +
+  theme(plot.title = element_text(hjust = 0.29, family = "Puritan", size = 15, color="black"),
+        plot.subtitle = element_text(hjust = 1, family = "Puritan", color="white")) +
+  theme(axis.text.x = element_text(colour = "gray21", 
+                                   face = "bold", family = "Dynalight", size = 15),
+        axis.text.y = element_text(vjust = -0.01,
+                                   hjust = 0.05,
+                                   margin = margin(l = 40, r = -60), colour = "white", 
+                                   face = "bold", family = "Puritan", size = 12)) +
+  theme(axis.title.x = element_text(color="black")) +
+  theme(axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank()) +
+  theme(legend.position = "none") +
+  theme(panel.border = element_rect(fill = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white", colour = "white"),
+        plot.background = element_rect(fill = "white", colour = "white"))
+
+dev.off()
+
+
+# reveal year
+
+png(here::here("Continued Plot Work", "Total Population and White and Negro Elements - Henry Gannett", "3-RevealYear-TotalPWN-Pop.png"),width=900,height=700)
+
+ggplot(df2, aes(x = value2, y = reorder(year2, -value2), fill="white"))  +
+  geom_vline(xintercept = 0:70 , linetype="solid", 
+             color = "gray21", size = 0.6) +
+  geom_vline(xintercept = c(10,20,30,40,50,60,70), linetype="solid", 
+             color = "gray21", size = 1.2) +
+  geom_col(position = position_stack(reverse = TRUE), colour = "black", width = 0.5) +
+  scale_fill_manual(values = c("white", "white")) +
+  scale_y_discrete() +
+  scale_x_continuous(labels = x_axis_text, expand = c(0,0), position = "top", limits = c(-8,70), 
+                     breaks = seq(-10, 70, by = 10)) +
+  labs(title = "TOTAL POPULATION",
+       subtitle = "PLATE I.") +
+  labs(y = NULL, x = "Millions.") +
+  theme(plot.title = element_text(hjust = 0.29, family = "Puritan", size = 15, color="black"),
+        plot.subtitle = element_text(hjust = 1, family = "Puritan", color="white")) +
+  theme(axis.text.x = element_text(colour = "gray21", 
+                                   face = "bold", family = "Dynalight", size = 15),
+        axis.text.y = element_text(vjust = -0.01,
+                                   hjust = 0.05,
+                                   margin = margin(l = 40, r = -60), colour = "gray21", 
+                                   face = "bold", family = "Puritan", size = 12)) +
+  theme(axis.title.x = element_text(color="black")) +
+  theme(axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank()) +
+  theme(legend.position = "none") +
+  theme(panel.border = element_rect(fill = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white", colour = "white"),
+        plot.background = element_rect(fill = "white", colour = "white"))
+
+dev.off()
+
+
+# reveal title
+
+png(here::here("Continued Plot Work", "Total Population and White and Negro Elements - Henry Gannett", "4-RevealTitle-TotalPWN-Pop.png"),width=900,height=700)
+
+ggplot(df2, aes(x = value2, y = reorder(year2, -value2), fill="white"))  +
+  geom_vline(xintercept = 0:70 , linetype="solid", 
+             color = "gray21", size = 0.6) +
+  geom_vline(xintercept = c(10,20,30,40,50,60,70), linetype="solid", 
+             color = "gray21", size = 1.2) +
+  geom_col(position = position_stack(reverse = TRUE), colour = "black", width = 0.5) +
+  scale_fill_manual(values = c("white", "white")) +
+  scale_y_discrete() +
+  scale_x_continuous(labels = x_axis_text, expand = c(0,0), position = "top", limits = c(-8,70), 
+                     breaks = seq(-10, 70, by = 10)) +
+  labs(title = "TOTAL POPULATION AND WHITE AND NEGRO ELEMENTS.",
+       subtitle = "PLATE I.") +
+  labs(y = NULL, x = "Millions.") +
+  theme(plot.title = element_text(hjust = 0.5, family = "Puritan", size = 15),
+        plot.subtitle = element_text(hjust = 1, family = "Puritan", color="gray21")) +
+  theme(axis.text.x = element_text(colour = "gray21", 
+                                   face = "bold", family = "Dynalight", size = 15),
+        axis.text.y = element_text(vjust = -0.01,
+                                   hjust = 0.05,
+                                   margin = margin(l = 40, r = -60), colour = "gray21", 
+                                   face = "bold", family = "Puritan", size = 12)) +
+  theme(axis.title.x = element_text(color="black")) +
+  theme(axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank()) +
+  theme(legend.position = "none") +
+  theme(panel.border = element_rect(fill = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white", colour = "white"),
+        plot.background = element_rect(fill = "white", colour = "white"))
+
+dev.off()
