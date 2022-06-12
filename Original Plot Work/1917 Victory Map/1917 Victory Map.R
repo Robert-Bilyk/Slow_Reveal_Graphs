0d8e1a0764c2baf204d76e1af336ad45869f4c1f
Robert Bilyk <bilykr@mail.gvsu.edu>
2022-06-12 17:30:03 +0000

    finalizing 1917 Victory Map

--- Original Plot Work/1917 Victory Map/1917 Victory Map.R
+++ Original Plot Work/1917 Victory Map/1917 Victory Map.R
@@ -36,6 +36,12 @@ newmap$Type <- newmap$Type %>%
                     'MUNICIPAL \nSUFFRAGE','MUNICIPAL \nSUFFRAGE IN \nCHARTER CITIES','NO \nSUFFRAGE'))
 
 
+#NOTE: "x cross dot.jpg" and "x cross pattern.jpg" images in the "1917 Victory Map" folder need to be copied into the "img" folder 
+#in the ggpattern directory the path is "R/x86_64-pc-linux-gnu-library/4.1/ggpattern/img"
+
+
+#Final Map
+
 png(here::here("Original Plot Work", "1917 Victory Map", "Victory_Map.png"),width=900,height=600)
 
 ggplot(data = newmap) + 
@@ -55,8 +61,8 @@ ggplot(data = newmap) +
   theme(legend.position = "bottom",
         legend.title = element_blank(),
         legend.text = element_text(size = 6, family = "Main"),
-        legend.key.width = unit(1.8, "cm"),
-        legend.key.height = unit(.9, "cm"),
+        legend.key.width = unit(1.6, "cm"),
+        legend.key.height = unit(.8, "cm"),
         legend.direction = "horizontal") +
   theme(axis.title = element_blank(),
         axis.text = element_blank(),
@@ -69,7 +75,7 @@ ggplot(data = newmap) +
   scale_fill_manual(values = c("white","white","black","white","white","white","white", "black")) +
   scale_pattern_manual(values = c("none", "none", "circle", "circle", "image", "image", "stripe", "none")) +
   scale_pattern_fill_manual(values = c("white", "white", "white", "black", "white", "white", "black", "white")) +
-  scale_pattern_spacing_manual(values = c(.05, .05, .025, .025, .05, .05, .006, .05)) +
+  scale_pattern_spacing_manual(values = c(.05, .05, .02, .02, .05, .05, .006, .05)) +
   scale_pattern_type_manual(values = c(NA, NA, NA, NA, "tile", "tile", NA, NA)) +
   scale_pattern_filename_manual(values = c(NA, NA, NA, NA, x_dot, x_cross, NA, NA)) +
   scale_pattern_scale_manual(values = c(1, 1, 1, 1, .15, .15, 1, 1)) +
@@ -77,31 +83,102 @@ ggplot(data = newmap) +
   scale_pattern_angle_manual(values = c(30, 30, 30, 30, 30, 30, 0, 30)) +
   guides(fill = guide_legend(nrow = 1))
 
-grid.draw(textGrob("The Woman Citizen", x=.4,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("WASH. \n1910", x=.13,y=.805, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("OREG. \n1912", x=.125,y=.7, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("CAL. \n1911", x=.125,y=.5, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("IDAHO \n1896", x=.22,y=.68, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("NEV. \n1914", x=.18,y=.56, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("UTAH. \n1896", x=.26,y=.56, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("MONT. \n1914", x=.295,y=.795, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("ARIZ. \n1912", x=.26,y=.415, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("WYO. \n1869", x=.325,y=.67, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
-grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Title")))
+grid.draw(textGrob("The Woman Citizen", x=.4,y=.98, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("November 24, 1917", x=.6,y=.98, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("WASH. \n1910", x=.13,y=.81, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("OREG. \n1912", x=.125,y=.7, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("CAL. \n1911", x=.125,y=.505, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("IDAHO \n1896", x=.22,y=.69, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("NEV. \n1914", x=.18,y=.57, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("UTAH. \n1896", x=.26,y=.565, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("MONT. \n1914", x=.295,y=.8, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("ARIZ. \n1912", x=.26,y=.42, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("WYO. \n1869", x=.325,y=.68, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("COL. \n1893", x=.355,y=.555, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("KAN. \n1912", x=.465,y=.545, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("ARK. \n1917", x=.555,y=.435, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .345, y= .425, width = .045, height = .025))
+grid.draw(textGrob("N.MEX.", x=.345,y=.425, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .45, y= .35, width = .03, height = .025))
+grid.draw(textGrob("TEX.", x=.45,y=.35, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .48, y= .45, width = .04, height = .025))
+grid.draw(textGrob("OKLA.", x=.48,y=.45, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .44, y= .635, width = .031, height = .04, gp = gpar(col="white")))
+grid.draw(textGrob("NEB. \n1917", x=.44,y=.635, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .44, y= .725, width = .045, height = .025))
+grid.draw(textGrob("S.DAK.", x=.44,y=.725, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .435, y= .81, width = .046, height = .04, gp = gpar(col="white")))
+grid.draw(textGrob("N.DAK. \n1917", x=.435,y=.81, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .525, y= .785, width = .04, height = .025))
+grid.draw(textGrob("MINN", x=.525,y=.785, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .535, y= .65, width = .04, height = .025))
+grid.draw(textGrob("IOWA.", x=.535,y=.65, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .55, y= .545, width = .03, height = .025))
+grid.draw(textGrob("MO.", x=.55,y=.545, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .555, y= .32, width = .03, height = .025))
+grid.draw(textGrob("LA.", x=.555,y=.32, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .605, y= .59, width = .03, height = .04, gp = gpar(col="white")))
+grid.draw(textGrob("ILL. \n1913", x=.605,y=.59, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .59, y= .73, width = .03, height = .025))
+grid.draw(textGrob("WIS.", x=.59,y=.73, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .675, y= .68, width = .04, height = .04, gp = gpar(col="white")))
+grid.draw(textGrob("MICH. \n1917", x=.675,y=.68, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .65, y= .605, width = .03, height = .04))
+grid.draw(textGrob("IND. \n1917", x=.65,y=.605, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .705, y= .6, width = .04, height = .04))
+grid.draw(textGrob("OHIO. \n1917", x=.705,y=.6, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .675, y= .515, width = .03, height = .025))
+grid.draw(textGrob("KY.", x=.675,y=.515, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .65, y= .46, width = .04, height = .025))
+grid.draw(textGrob("TENN.", x=.65,y=.46, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .595, y= .37, width = .04, height = .025))
+grid.draw(textGrob("MISS.", x=.595,y=.37, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .64, y= .375, width = .03, height = .025))
+grid.draw(textGrob("ALA.", x=.64,y=.375, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .69, y= .375, width = .03, height = .025))
+grid.draw(textGrob("GA.", x=.69,y=.375, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .71, y= .27, width = .03, height = .025))
+grid.draw(textGrob("FLA.", x=.71,y=.27, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .735, y= .405, width = .03, height = .025))
+grid.draw(textGrob("S.C.", x=.735,y=.405, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .755, y= .46, width = .03, height = .025))
+grid.draw(textGrob("N.C.", x=.755,y=.46, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .765, y= .515, width = .03, height = .025))
+grid.draw(textGrob("VA.", x=.765,y=.515, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .738, y= .55, width = .04, height = .025))
+grid.draw(textGrob("W.VA.", x=.738,y=.55, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .8, y= .55, width = .03, height = .025))
+grid.draw(textGrob("M.D.", x=.8,y=.55, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("DEL.", x=.835,y=.548, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .825, y= .59, width = .03, height = .025))
+grid.draw(textGrob("N.J.", x=.825,y=.59, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .78, y= .61, width = .04, height = .025))
+grid.draw(textGrob("PENN.", x=.78,y=.61, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("N.Y. \n1917", x=.82,y=.67, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("CONN.", x=.858,y=.61, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("R.I. \n1917", x=.89,y=.605, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("MASS.", x=.91,y=.655, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .875, y= .7, width = .03, height = .025))
+grid.draw(textGrob("N.H.", x=.875,y=.7, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(rectGrob(x= .91, y= .75, width = .03, height = .025))
+grid.draw(textGrob("ME.", x=.91,y=.75, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(textGrob("VER. \n1917", x=.86,y=.76, gp=gpar(fontsize=8, fontfamily="Main")))
+grid.draw(circleGrob(x=.715,y=.235, r=.0065))
+grid.draw(circleGrob(x=.715,y=.235, r=.0005))
+grid.draw(circleGrob(x=.705,y=.565, r=.0065))
+grid.draw(circleGrob(x=.705,y=.565, r=.0005))
+grid.draw(textGrob("?", x=.65,y=.565, gp=gpar(fontsize=20, fontfamily="Main", col = "white")))
+
+grid.draw(segmentsGrob(x0 = 0.550, x1 = 0.558, y0 = 0.4, y1 = 0.4))
+grid.draw(segmentsGrob(x0 = 0.554, x1 = 0.554, y0 = 0.394, y1 = 0.406))
+grid.draw(segmentsGrob(x0 = 0.551, x1 = 0.557, y0 = 0.395, y1 = 0.405))
+grid.draw(segmentsGrob(x0 = 0.551, x1 = 0.557, y0 = 0.405, y1 = 0.395))
+
+grid.draw(segmentsGrob(x0 = 0.056, x1 = 0.064, y0 = 0.045, y1 = 0.045))
+grid.draw(segmentsGrob(x0 = 0.06, x1 = 0.06, y0 = 0.039, y1 = 0.051))
+grid.draw(segmentsGrob(x0 = 0.057, x1 = 0.063, y0 = 0.04, y1 = 0.05))
+grid.draw(segmentsGrob(x0 = 0.057, x1 = 0.063, y0 = 0.05, y1 = 0.04))
+
 
 
 dev.off()
